b4a5aa36083ee4e3249150c105d70d4e4fce79ec
Noel Worden <noelworden@hey.com>
2023-09-22 12:34:57 -0600

    [TOOL] Add Credo and refac where necessary

--- lib/shorten_it_web/components/layouts.ex
+++ lib/shorten_it_web/components/layouts.ex
@@ -1,4 +1,5 @@
 defmodule ShortenItWeb.Layouts do
+  @moduledoc false
   use ShortenItWeb, :html
 
   embed_templates "layouts/*"
--- lib/shorten_it_web/telemetry.ex
+++ lib/shorten_it_web/telemetry.ex
@@ -1,4 +1,5 @@
 defmodule ShortenItWeb.Telemetry do
+  @moduledoc false
   use Supervisor
   import Telemetry.Metrics
 
